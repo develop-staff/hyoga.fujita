a741ac05abf955c44b805cc0e2cb55d696787441
fujita <fujita@sutaffunoMacBook-puro.local>
2018-02-26 15:47:10 +0900

    Thymekle

---
+++ indexForm_1data/src/main/java/com/example/song_html_form/Song.java
@@ -0,0 +1,10 @@
+package com.example.song_html_form;
+
+import lombok.Data;
+
+import java.io.File;
+
+@Data
+public class Song {
+    String fileName;
+}
---
+++ indexForm_1data/src/main/java/com/example/song_html_form/SongHtmlFormApplication.java
@@ -0,0 +1,12 @@
+package com.example.song_html_form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongHtmlFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongHtmlFormApplication.class, args);
+	}
+}
